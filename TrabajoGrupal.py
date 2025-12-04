3ef513525c0a4c72ed2d644575e08bf105a20951
Solft <fabielias72@gmail.com>
2025-12-04 18:55:20 +0100

    cambio de autor

--- TrabajoGrupal.py
+++ TrabajoGrupal.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Dec  4 17:57:50 2025
-@author: pablo, subire, Mohammed
+@author: pablo, subire, Mohammed, Fabian
 """
 import tkinter as tkinter
 raiz = tkinter.Tk()
