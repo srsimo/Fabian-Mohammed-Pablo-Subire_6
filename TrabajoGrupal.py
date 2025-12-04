4383fe5e11b8781e805e1841518174e8d09ff754
pfosela <pablo.fosela@gmail.com>
2025-12-04 18:46:57 +0100

    Creacion del archivo y borrado de mi actividad

---
+++ TrabajoGrupal.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  4 17:57:50 2025
+@author: pablo
+"""
+import tkinter as tkinter
+raiz = tkinter.Tk()
+raiz.title("UDUU")
+raiz.geometry("800x600")
+
+# Configurar que las columnas se expandan proporcionalmente
+raiz.grid_columnconfigure(0, weight=1)
+raiz.grid_columnconfigure(1, weight=1)
+raiz.grid_columnconfigure(2, weight=1)
+raiz.grid_columnconfigure(3, weight=1)
+raiz.grid_columnconfigure(4, weight=1)
+
+textoCrear = "Crear"
+botonCrear = tkinter.Button(raiz, text=textoCrear, relief="solid", bd=1, highlightbackground="black", highlightthickness=1)
+botonCrear.grid(row=0, column=0, sticky="ew")
+botonCrear.config(fg="white", bg="dodgerblue", font=('arial',15))
+
+textoBorrar = "Eliminar"
+botonBorrar = tkinter.Button(raiz, text=textoBorrar, relief="solid", bd=1, highlightbackground="black", highlightthickness=1)
+botonBorrar.grid(row=0, column=1, sticky="ew")
+botonBorrar.config(fg="white", bg="dodgerblue", font=('arial',15))
+
+textoEditar = "Editar"
+botonEditar = tkinter.Button(raiz, text=textoEditar, relief="solid", bd=1, highlightbackground="black", highlightthickness=1)
+botonEditar.grid(row=0, column=2, sticky="ew")
+botonEditar.config(fg="white", bg="dodgerblue", font=('arial',15))
+
+textoBuscar = "Buscar"
+botonBuscar = tkinter.Button(raiz, text=textoBuscar, relief="solid", bd=1, highlightbackground="black", highlightthickness=1)
+botonBuscar.grid(row=0, column=3, sticky="ew")
+botonBuscar.config(fg="white", bg="dodgerblue", font=('arial',15))
+
+textoGrafico = "Grafico"
+botonGrafico = tkinter.Button(raiz, text=textoGrafico, relief="solid", bd=1, highlightbackground="black", highlightthickness=1)
+botonGrafico.grid(row=0, column=4, sticky="ew")
+botonGrafico.config(fg="white", bg="dodgerblue", font=('arial',15))
+
+raiz.mainloop()
